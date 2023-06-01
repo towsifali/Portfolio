3ba66f9b8d02e9bb9758daa8bf8ddda63dbe153e
Towsif <ali.towsif1@gmail.com>
2023-06-01 17:38:42 +0800

    Content Section Font Fixed

--- components/ContentSection.tsx
+++ components/ContentSection.tsx
@@ -9,11 +9,11 @@ type ContentSectionProps = {
 const ContentSection = () => {
   return (
     <div>
-        <div className="flex-row">
-            <p className="text-xl text-left font-semibold">Software Dev Intern</p>
-            <p className="text-lg text-right italic">Hospital Authority</p>
+        <div className="flex flex-row justify-between">
+            <div className="text-xl text-left font-semibold">Software Dev Intern</div>
+            <div className="text-xl text-right italic">Hospital Authority</div>
         </div>
-        <p className="text-lg">2022 JUL - Present</p>
+        <p className="text-base">2022 JUL - Present</p>
         <div>
             <ul className="list-disc">
                 <li>Developed a full-stack web application for handling private cloud usage and user information databases and using FastAPI, MySQL, and React.Js</li>
