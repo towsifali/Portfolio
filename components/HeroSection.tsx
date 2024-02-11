9bf29f1537da748cc5ac1ab0fbd99cb4e288ade2
Towsif <ali.towsif1@gmail.com>
2024-02-12 05:28:24 +0800

    p tag test vercel

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -63,10 +63,8 @@ export default function HeroSection() {
           offset={-100}
           duration={500}
         >
-          <>
-            <BiCodeAlt size={25} className="mr-0.5" />
-            Projects
-          </>
+          <BiCodeAlt size={25} className="mr-0.5" />
+          <p>Projects</p>
         </Link>
         <button
           className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
